4f314985a53e59b34a7b37514e3d09c469925838
Magnus Glantz <sudo@redhat.com>
2017-11-18 19:48:19 +0100

    Removed NFS logic from masterPrep

--- scripts/masterPrep.sh
+++ scripts/masterPrep.sh
@@ -112,40 +112,4 @@ fi
 systemctl enable docker
 systemctl start docker
 
-# Prereqs for NFS, if we're $MASTER-0
-# Create a lv with what's left in the docker-vg VG, which depends on disk size defined (100G disk = 60G free)
-
-if hostname -f|grep -- "-0" >/dev/null
-then
-   echo $(date) " - We are on master-0 ($(hostname)): Setting up NFS server for persistent storage"
-   yum -y install nfs-utils
-   VGCALC=$(vgs|grep docker-vg|awk '{ print $7 }'|sed -e 's/.[0-9][0-9]g//' -e 's/<//g')
-   VGFREESPACE=$(echo $VGCALC - 1|bc)
-   lvcreate -n lv_nfs -L+$VGFREESPACE docker-vg
-   mkfs.xfs /dev/mapper/docker--vg-lv_nfs
-   echo "/dev/mapper/docker--vg-lv_nfs /exports xfs defaults 0 0" >>/etc/fstab
-   mkdir /exports
-   mount -a
-   if [ "$?" -eq 0 ]
-   then
-      echo "$(date) Successfully setup NFS."
-   else
-      echo "$(date) Failed to mount filesystem which is to host the NFS share."
-      exit 6
-   fi
-   
-   lvextend -l 100%FREE /dev/docker-vg/lv_nfs
-   xfs_growfs /dev/docker-vg/lv_nfs
-   
-   for item in registry metrics jenkins
-   do 
-      mkdir -p /exports/$item
-   done
-   
-   chown nfsnobody:nfsnobody /exports -R
-   chmod a+rwx /exports -R  
-fi
-
-
-
 echo $(date) " - Script Complete"
